31d3c10e4b4fb701959b10fe2539b600d63f25c9
Evgen <evgeniikupitmanuk@gmail.com>
2021-11-24 15:59:12 +0300

    update

--- Short Long Short.js
+++ Short Long Short.js
@@ -1,4 +1,8 @@
-function solution(a, b)  {
-    return a.length < b.length ? a + b + a : b + a + b
+function solution(a, b) {
+    if (a.length < b.length) {
+        return a + b + a
+    } else {
+        return b + a + b
+    }
 }
 solution("1", "22")
