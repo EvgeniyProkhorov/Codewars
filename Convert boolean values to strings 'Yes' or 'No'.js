66e16b97416bf312d8d4fde9ec1236b6cd714b66
EvgeniyProkhorov <firerequest@gmail.com>
2021-11-25 19:44:21 +0300

    upd

---
+++ Convert boolean values to strings 'Yes' or 'No'.js
@@ -0,0 +1,15 @@
+function boolToWord( bool ){
+  if (bool == true) {
+    return "Yes";
+  } else {
+    return "No";
+  }
+}
+
+
+OR
+
+
+function boolToWord( bool ){
+  return bool == true ? "Yes" : "No";
+}
