077265bdbae41b4ed27fd6c56867bf29f0a93e64
EvgeniyProkhorov <firerequest@gmail.com>
2021-11-26 01:18:04 +0300

    Upd

---
+++ Find the smallest integer in the array.js
@@ -0,0 +1,11 @@
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    let small = args[0]
+
+    for(let i = 0; i < args.length; i++){
+        if(args[i] < small) small = args[i]
+    }
+
+    return small
+}
+  }
